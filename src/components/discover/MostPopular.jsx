22b73e52a0634179600d83377709c34f7bdd657e
Dhruv <dhruvanandsingh2004@gmail.com>
2026-02-01 04:02:00 +0530

    Fix: Add missing API import to discover components

--- src/components/discover/MostPopular.jsx
+++ src/components/discover/MostPopular.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { IoStarOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../config/api";
 
 function UniversityCard({ image, name, location, stars, id, views }) {
   const navigate = useNavigate();
